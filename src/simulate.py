32913a5be4bf541ff4a52af1de7c9438e18b3380
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-04-28 14:16:04 +0100

    Added visualisation to gluber model sim

--- src/simulate.py
+++ src/simulate.py
@@ -13,6 +13,15 @@ class simulate(object):
         self.N= N
         self.kT= kT
         self.algType= algType
+        self.timestep= 0
+        self.sweep= 1
+       
+        # 1 sweep ==> 2500 timesteps
+        self.timestepLimit= 2500
+
+        # Update visualisation every 10 sweeps
+        self.visPeriod= 10
+        
 
     def generateInitLattice(self):
         #========================================================
@@ -34,4 +43,29 @@ class simulate(object):
 
         self.lattice= np.ones((self.N, self.N))
         self.lattice[int(self.N/4):int(3*self.N/4), int(self.N/4):int(3*self.N/4)]= -1
+
+    def sweepUpdate(self):
+        #========================================================
+        # Update sweep value according to the current value of timestep variable
+
+        if self.timestep == self.timestepLimit:
+            self.sweep += 1
+            self.timestep = 0
+        
+    def runGlauberSimulation(self):
+        #========================================================
+        # Run simulation of glauber algorithm with visualisation
+
+        self.generateInitLattice()
+        self.animation= animate(self.lattice)
+        algorithmClass= algorithms(self.N, self.kT)
+        while True:
+            algorithmClass.glauberStep(self.lattice)
+            self.timestep+=1
+            self.sweepUpdate
+            if self.sweep % self.visPeriod == 0:
+                self.animation.draw_image(self.lattice)
+
+
+
 
